d2df35fd1423730037d5c711ebdccb41a4cb97e3
Monalisa <monali291944@gmail.com>
2025-01-12 05:03:04 +0530

    11th Jan

---
+++ src/ex_02_Java_Basics/Lab002_Variables.java
@@ -0,0 +1,33 @@
+package ex_02_Java_Basics;
+
+import java.sql.SQLOutput;
+
+public class Lab002_Variables {
+    public static void main(String[] args) {
+
+        int a = 1;
+        System.out.printf("9 x 1 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 2 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 3 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 4 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 5 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 6 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 7 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 8 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 9 = %d", 9 * a++);
+        System.out.println();
+        System.out.printf("9 x 10 = %d", 9 * a++);
+        System.out.println();
+
+
+
+    }
+}
